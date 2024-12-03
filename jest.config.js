31458d2471a4434838fe5f43fd157b0651f1c46c
adimalka14 <122941157+adimalka14@users.noreply.github.com>
2024-12-03 17:39:26 +0200

    plan the game

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+
+const config = {
+    // Indicates which provider should be used to instrument code for coverage
+    coverageProvider: "v8",
+    // A map from regular expressions to paths to transformers
+    transform: {
+        "^.+\\.jsx?$": "babel-jest",
+    },
+};
+
+module.exports = config;
